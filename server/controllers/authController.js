86c46cf9b41626a1e0c5f08af9a4986c44f86962
ameer017 <rajiabdullahi907@gmail.com>
2024-09-19 22:48:17 +0100

    email verification function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -58,22 +58,24 @@ exports.createUser = asyncHandler(async (req, res) => {
             to: newUser.emailAddress,
             subject: "Account created Successfully",
             html: `
-                Hi ${newUser.fullName},\n
-                We're excited to have you on board!
+                <p>Hi ${newUser.fullName},</p>
 
-                Your account has been successfully created on user-auth-starter-kit, and you're now ready to explore all the features we offer.
+                    <p>We're excited to have you on board!</p>
 
-                Here's a summary of your account details:
-                
-                Email: ${newUser.emailAddress}
-               Phone Number: ${newUser.phone_no}
+                 <p>Your account has been successfully created on <strong>user-auth-starter-kit</strong>, and you're now ready to explore all the features we offer.</p>
 
-                If you ever have any questions or need assistance, don't hesitate to reach out to our support team at [Support Email]. We're here to help!
+                    <p>Here's a summary of your account details:</p>
+                    <ul>
+                        <li>Email: ${newUser.emailAddress}</li>
+                        <li>Phone Number: ${newUser.phone_no}</li>
+                    </ul>
 
-                Thank you for joining us. We're looking forward to seeing you thrive on our platform.
+                    <p>If you ever have any questions or need assistance, don't hesitate to reach out to our support team at <a href="mailto:support@example.com">[Support Email]</a>. We're here to help!</p>
 
-                Best regards,
-                user-auth-starter-kit
+                 <p>Thank you for joining us. We're looking forward to seeing you thrive on our platform.</p>
+
+                    <p>Best regards,<br>
+                    <strong>user-auth-starter-kit</strong></p>
                 
                 `
         })
@@ -181,9 +183,42 @@ exports.logoutUser = asyncHandler(async (req, res) => {
     return res.status(200).json({ message: "User logged out successfully!" })
 })
 
-exports.getUser = asyncHandler(async (req, res) => { })
+exports.getUser = asyncHandler(async (req, res) => {
+    //  get a single user by their id
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        const { _id,
+            fullName,
+            emailAddress,
+            phone_no,
+            address,
+            verified,
+            role,
+            gender,
+            picture } = user;
+
+        res.status(200).json({
+            _id,
+            fullName,
+            emailAddress,
+            phone_no,
+            address,
+            verified,
+            role,
+            gender,
+            picture
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found!")
+    }
+
+
+})
 
 exports.getUsers = asyncHandler(async (req, res) => {
+    //  Find and get all available users, arrange them in the order of their creation [last to be created] and do not include their password
     const users = await User.find().sort("-createdAt").select("-password").exec();
 
     if (!users) {
@@ -194,9 +229,138 @@ exports.getUsers = asyncHandler(async (req, res) => {
     return res.status(200).json(users);
 })
 
-exports.upgradeUser = asyncHandler(async (req, res) => { })
-exports.updateUser = asyncHandler(async (req, res) => { })
-exports.verifyUser = asyncHandler(async (req, res) => { })
-exports.deleteUser = asyncHandler(async (req, res) => { })
+exports.upgradeUser = asyncHandler(async (req, res) => {
+    //  Properties needed for the upgrade, new role and the user's id
+    const { role, id } = req.body;
+
+    //  fetch the user with the id
+    const user = await User.findById(id);
+
+    if (!user) {
+        res.status(404);
+        throw new Error("User not found!")
+    }
+
+    // Role specified is now the user's role
+    user.role = role;
+    await user.save();
+
+    // return output in json format.
+    res.status(200).json({
+        message: `User role updated to ${role}`,
+    });
+})
+
+exports.updateUser = asyncHandler(async (req, res) => {
+    //  fidn user by their id
+    const user = await User.findById(req.user._id);
+
+    // Properties to be updated
+    const { fullName, phone_no, address, picture } = user;
+
+    //  provided data or their details remains
+    user.fullName = req.body.fullName || fullName
+    user.phone_no = req.body.phone_no || phone_no;
+    user.address = req.body.address || address;
+    user.picture = req.body.picture || picture;
+
+    // save the new details if there is
+    const updatedUser = await user.save();
+
+    res.json({
+        _id: updatedUser._id,
+        fullName: updatedUser.fullName,
+        emailAddress: updatedUser.emailAddress,
+        phone_no: updatedUser.phone_no,
+        address: updatedUser.address,
+        verified: updatedUser.verified,
+        role: updatedUser.role,
+        gender: updatedUser.gender,
+        picture: updatedUser.picture,
+    })
+
+})
+
+exports.sendVerificationEmail = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        res.status(404);
+        throw new Error("User not found");
+    }
+
+    if (user.isVerified) {
+        res.status(400);
+        throw new Error("User already verified");
+    }
+
+    // Delete Token if it exists in DB
+    let token = await Token.findOne({ userId: user._id });
+    if (token) {
+        await token.deleteOne();
+    }
+
+    //   Create Verification Token and Save
+    const verificationToken = crypto.randomBytes(32).toString("hex") + user._id;
+    console.log(verificationToken)
+
+    // Hash token and save
+    const hashedToken = hashToken(verificationToken);
+    await new Token({
+        userId: user._id,
+        vToken: hashedToken,
+        createdAt: Date.now(),
+        expiresAt: Date.now() + 60 * (60 * 1000), // 60mins
+    }).save();
+
+    // Construct Verification URL
+    const verificationUrl = `${process.env.FRONTEND_URL}/verify/${verificationToken}`;
+
+    await sendMail({
+        from: process.env.EMAIL_USER,
+        to: user.emailAddress,
+        subject: "Verify Your Account",
+        html: `
+           <p>Dear ${user.fullName},</p>
+
+            <p>Welcome to <strong>User-auth-starter-kit</strong>! We're excited to have you on board.</p>
+
+            <p>Before you can start exploring all the amazing features we offer, please verify your email address by clicking the button below:</p>
+
+            <p><a href='${verificationUrl}' style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px;">Verify Your Account</a></p>
+
+            <p>Or, you can copy and paste the following link into your browser:</p>
+
+            <p>
+            '${verificationUrl}'
+            </p>
+
+            <p>Verifying your account ensures the security of your profile and helps us provide you with a personalized experience.</p>
+
+            <p>If you did not create this account, please ignore this email.</p>
+
+            <p>Thank you for joining <strong>User-auth-starter-kit</strong>!</p>
+
+            <p>Best regards,<br>
+            The <strong>User-auth-starter-kit</strong> Team</p>
+        `
+    })
+})
+
+exports.verifyUser = asyncHandler(async (req, res) => {
+
+})
+
+exports.deleteUser = asyncHandler(async (req, res) => {
+    const userToDelete = await User.findById(req.params.id);
+
+    if (!userToDelete) {
+        res.status(404);
+        throw new Error("User not found");
+    }
+
+    await userToDelete.deleteOne();
+    return res.status(200).json({ message: "User deleted successfully!" })
+})
 exports.resetPassword = asyncHandler(async (req, res) => { })
 exports.loginWithGoogle = asyncHandler(async (req, res) => { })
--- server/route/auth.js
+++ server/route/auth.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const { createUser, loginUser, logoutUser, getUsers } = require("../controllers/authController");
-const { protect } = require("../middleware/authMiddleware");
+const { createUser, loginUser, logoutUser, getUsers, deleteUser, getUser, upgradeUser, sendVerificationEmail } = require("../controllers/authController");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
 const route = express.Router()
 
 route.post("/create-account", createUser)
 route.post("/login-user", loginUser)
-route.post("/logout-user", protect,  logoutUser)
+route.post("/logout-user", protect, logoutUser)
+route.post("/upgrade-user", protect, adminOnly, upgradeUser)
+route.patch("/update-user", protect, upgradeUser)
+route.post("/sendVerificationEmail", protect, sendVerificationEmail);
 
 
-route.get("/get-users", protect,  getUsers)
+route.get("/get-users", protect, getUsers)
+
+
+route.delete("/delete-user/:id", protect, adminOnly, deleteUser)
+route.get("/get-user/:id", protect, getUser)
+
 
 module.exports = route;

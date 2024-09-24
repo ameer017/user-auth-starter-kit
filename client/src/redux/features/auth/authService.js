c8319a03fd2e17b9216f12654383d37d9b5fbddd
ameer017 <rajiabdullahi907@gmail.com>
2024-09-24 20:53:45 +0100

    backend-route-initialized

--- client/src/redux/features/auth/authService.js
+++ client/src/redux/features/auth/authService.js
@@ -2,57 +2,77 @@ import axios from "axios"
 const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
 const URL = `${BACKEND_URL}/api/v1/auth/`;
 
-const createUser = async (userData) => {
+exports.createUser = async (userData) => {
     const response = await axios.post(`${URL}/create-account`, userData);
     return response.data;
 }
 
-const loginUser = async (userData) => {
+exports.loginUser = async (userData) => {
     const response = await axios.post(`${URL}/login-user`, userData);
     return response.data;
 }
 
-const logoutUser = async () => {
+exports.logoutUser = async () => {
     const response = await axios.post(`${URL}/logout-user`);
     return response.data.message;
 }
 
-const upgradeUser = async (userData) => {
+exports.upgradeUser = async (userData) => {
     const response = await axios.post(`${URL}/upgrade-user`, userData);
     return response.data.message;
 }
 
-const updateUser = async (userData) => {
+exports.updateUser = async (userData) => {
     const response = await axios.patch(`${URL}/update-user`, userData);
     return response.data;
 }
 
-const sendVerificationEmail = async () => {
+exports.sendVerificationEmail = async () => {
     const response = await axios.post(`${URL}/send-verification-email`);
     return response.data.message;
 }
 
-const verifyUser = async (verificationToken) => {
+exports.verifyUser = async (verificationToken) => {
     const response = await axios.patch(`${URL}/verify-user/${verificationToken}`);
     return response.data.message;
 }
 
-const resetPassword = async (userData, resetToken) => {
+exports.resetPassword = async (userData, resetToken) => {
     const response = await axios.patch(`${URL}/reset-password/${resetToken}`, userData);
     return response.data.message;
 }
 
-const changePassword = async (userData) => {
+exports.changePassword = async (userData) => {
     const response = await axios.patch(`${URL}/change-password }`, userData);
     return response.data.message;
 }
 
-const forgotPassword = async (userData) => {
+exports.forgotPassword = async (userData) => {
     const response = await axios.patch(`${URL}/forgot-password }`, userData);
     return response.data.message;
 }
 
-const loginWithGoogle = async (userData) => {
-    const response = await axios.patch(`${URL}/forgot-password }`, userData);
+exports.loginWithGoogle = async (userToken) => {
+    const response = await axios.post(`${URL}/google/callback }`, userToken);
+    return response.data;
+}
+
+exports.loginStatus = async () => {
+    const response = await axios.get(`${URL}/login-status`);
     return response.data.message;
+}
+
+exports.getUsers = async () => {
+    const response = await axios.get(`${URL}/get-users`);
+    return response.data;
+}
+
+exports.getUser = async (id) => {
+    const response = await axios.get(`${URL}/get-user/${id}`);
+    return response.data;
+}
+
+exports.deleteUser = async (id) => {
+    const response = await axios.get(`${URL}/delete-user/${id}`);
+    return response.data;
 }

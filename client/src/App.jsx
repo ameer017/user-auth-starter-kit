9242f5ffabc442cdce1f7105a6a373ee2f1257af
ameer017 <rajiabdullahi907@gmail.com>
2024-09-26 20:53:21 +0100

    setting-up-frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './components/Home/Home'
 import Layout from './components/Layout/Layout'
+import Profile from './page/profile/Profile'
+import ChangePassword from './page/auth/ChangePassword'
+import ResetPassword from './page/auth/ResetPassword'
+import Login from './page/auth/Login'
+import Register from './page/auth/Register'
 
 export default function App() {
   return (
@@ -14,6 +19,27 @@ export default function App() {
           </Layout>
 
         } />
+
+        <Route path='/register' element={<Layout>
+          <Register />
+        </Layout>} />
+
+        <Route path='/login' element={<Layout>
+          <Login />
+        </Layout>} />
+
+        <Route path='/profile' element={<Layout>
+          <Profile />
+        </Layout>} />
+
+        <Route path='/change-password' element={<Layout>
+          <ChangePassword />
+        </Layout>} />
+
+        <Route path='/reset-password' element={<Layout>
+          <ResetPassword />
+        </Layout>} />
+
       </Routes>
     </>
   )

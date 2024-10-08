16ef8eefe151cb3976a0fa25bcf0406fce94d97e
ameer017 <rajiabdullahi907@gmail.com>
2024-10-08 21:49:23 +0100

    reset-password

--- client/src/page/auth/ResetPassword.jsx
+++ client/src/page/auth/ResetPassword.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
+    <div>
+      <form>
+        <div></div>
+        <div></div>
+      </form>
+    </div>
   )
 }
 
